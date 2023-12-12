aab6ef81e51a0370b604cec35f63be2f95043600
k_95.21 <kohta0421kochi@gmail.com>
2023-12-12 17:01:08 +0900

    add all fiels

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,14 @@
-import React from 'react';
-import { Sidebar, Menu, MenuItem, useProSidebar } from 'react-pro-sidebar';
+'use client';
+import Header from "@/components/organisms/Header";
+import React from "react";
 
-// material icons import
-import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
-import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
-import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
-import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
-import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
-import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
-import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 
 // components import
 
 export default function Home() {
-  const { collapseSidebar } = useProSidebar();
   return (
-    <main className="flex">
-    </main>
+    <div id="app" >
+      <Header />
+    </div>
   );
 }
