7db96c4b21398179991e79a3b0ac0c4a0d9d85f0
k_95.21 <kohta0421kochi@gmail.com>
2023-12-12 16:24:27 +0900

    add sidebar pro files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Sidebar, Menu, MenuItem, useProSidebar } from 'react-pro-sidebar';
+
+// material icons import
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+
+// components import
 
 export default function Home() {
+  const { collapseSidebar } = useProSidebar();
   return (
-    <main className="">
-      <p className="px-4 text-xl text-amber-400">sample</p>
+    <main className="flex">
     </main>
   );
 }
