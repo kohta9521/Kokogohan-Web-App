7865f5a711253e3c582d5a7dd3dcf586a7706694
k_95.21 <kohta0421kochi@gmail.com>
2023-12-19 23:50:10 +0900

    add genre menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import React from "react";
 
 // components import
 import BasicLayout from "@/components/templates/BasicLayout";
+import GenreBox from "@/components/organisms/GenreBox";
 
 
 export default function Home() {
   return (
     <div id="app">
       <BasicLayout>
+        <GenreBox />
       </BasicLayout>
     </div>
   );
---
+++ src/components/organisms/GenreBox.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+// components import
+
+
+const GenreBox = () => {
+    return (
+        <div className="mx-auto max-w-7xl py-5 bg-white border-2 border-red-400 lg:px-8">
+            <h1>sample</h1>
+        </div>
+    )
+}
+
+export default GenreBox
