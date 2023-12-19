577191564bb5b6ffabd7489d2f3fcb2ed53a8aee
k_95.21 <kohta0421kochi@gmail.com>
2023-12-19 20:20:48 +0900

    add login screen basic layouts

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 // components import
@@ -5,9 +7,17 @@ import React from 'react'
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login Page</h1>
+        <div className="w-full flex">
+            {/* ロゴを追加 */}
+            <div className='flex items-center justify-center w-6/12 h-screen bg-red-600'>
+                <Image className="" width={300} height={70} src="/images/logo.png" alt="logoimg" />
+            </div>
+            <div className='w-6/12'>
+                <h1>ログイン画面</h1>
+                <Link href={"/"}>ホームに戻る</Link>
+            </div>
         </div>
+
     )
 }
 
